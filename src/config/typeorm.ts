589d2c31c130aec4a88967d622ff0a430bfd6244
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-05-13 23:00:48 -0300

    Changed dropschema: false

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -12,7 +12,7 @@ const config = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    dropSchema: true,
+    // dropSchema: true,
     synchronize: true,
     logging: true,
     autoLoadEntities: true,
